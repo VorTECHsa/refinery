a7f746a7d8209d267fd7b3e417806efd2afa7e74
Declan Crew <deaglancrew@googlemail.com>
2021-11-17 13:13:05 +0000

    Switch to regex matching for HeaderCell

--- src/main/java/com/vortexa/refinery/cell/AbstractHeaderCell.kt
+++ src/main/java/com/vortexa/refinery/cell/AbstractHeaderCell.kt
@@ -2,7 +2,7 @@ package com.vortexa.refinery.cell
 
 sealed class AbstractHeaderCell(private val patterns: List<String>) {
 
-    fun contains(s: String) = patterns.any { s.trim().lowercase().contains(it.lowercase()) }
+    fun contains(s: String) = patterns.any { it.toRegex(option = RegexOption.IGNORE_CASE).containsMatchIn(s) }
 
     fun inside(values: Set<String>) = values.any { this.contains(it) }
 }
