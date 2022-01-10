a8eda343c4395e340a50eeb60edded5c9d53cf51
stefbg <stefan.mihaylov@vortexa.com>
2022-01-10 15:05:37 +0000

    adding tests

--- src/main/java/com/vortexa/refinery/cell/parser/StringCellParser.kt
+++ src/main/java/com/vortexa/refinery/cell/parser/StringCellParser.kt
@@ -1,15 +1,16 @@
 package com.vortexa.refinery.cell.parser
 
 import org.apache.poi.ss.usermodel.Cell
+import org.apache.poi.ss.usermodel.CellType
 
 class StringCellParser : CellParser<String> {
 
     override fun tryParse(cell: Cell?): String? {
-        val value = cell?.toString()?.trim()
-        return if (value == null || value.isEmpty()) {
-            null
-        } else {
-            value
+        if (cell == null) return null
+        val value = when {
+            cell.getConcreteCellType() == CellType.STRING -> cell.stringCellValue.trim()
+            else -> cell.toString().trim()
         }
+        return if (value == null || value.isEmpty()) null else value
     }
 }
