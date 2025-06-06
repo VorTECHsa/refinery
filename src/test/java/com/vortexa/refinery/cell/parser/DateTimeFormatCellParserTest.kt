385dbb7ad1300a4f6cb588c781e5595da5448ffb
Michael Smith-Chandler <michael.smithchandler@vortexa.com>
2025-06-06 14:03:10 +0100

    Restore old CellParserException constructor

--- src/test/java/com/vortexa/refinery/cell/parser/DateTimeFormatCellParserTest.kt
+++ src/test/java/com/vortexa/refinery/cell/parser/DateTimeFormatCellParserTest.kt
@@ -1,6 +1,8 @@
 package com.vortexa.refinery.cell.parser
 
+import com.vortexa.refinery.exceptions.CellParserException
 import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.assertThatThrownBy
 import org.junit.jupiter.api.Test
 import java.time.LocalDate
 import java.time.LocalDateTime
@@ -12,29 +14,30 @@ class DateTimeFormatCellParserTest : CellParserTest() {
     fun `should parse date time with formatter`() {
         // expect
         val parser = DateTimeFormatCellParser(DateTimeFormatter.ofPattern("dd-MM-yyyy"))
-        assertThat(parser.tryParse(dateStr()))
+        assertThat(parser.parse(dateStr()))
             .isEqualTo(LocalDate.of(2021, 11, 15).atStartOfDay())
 
         val parser2 = DateTimeFormatCellParser(DateTimeFormatter.ofPattern("dd-MM-yyyy @ HH:mm"))
-        assertThat(parser2.tryParse(dateTimeStr()))
+        assertThat(parser2.parse(dateTimeStr()))
             .isEqualTo(LocalDateTime.of(2021, 11, 15, 15, 43))
     }
 
     @Test
-    fun `should return null if could not convert string to date time`() {
+    fun `should throw if could not convert string to date time`() {
         val parser = DateTimeFormatCellParser(DateTimeFormatter.ofPattern("dd-MM-yyyy"))
-        assertThat(parser.tryParse(stringCell())).isNull()
+        assertThatThrownBy { parser.parse(stringCell()) }
+            .isInstanceOf(CellParserException::class.java)
     }
 
     @Test
     fun `should parse date time with formatter from formula`() {
         // expect
         val parser = DateTimeFormatCellParser(DateTimeFormatter.ofPattern("dd-MM-yyyy"))
-        assertThat(parser.tryParse(dateStrFromFormula()))
+        assertThat(parser.parse(dateStrFromFormula()))
             .isEqualTo(LocalDate.of(2021, 11, 15).atStartOfDay())
 
         val parser2 = DateTimeFormatCellParser(DateTimeFormatter.ofPattern("dd-MM-yyyy @ HH:mm"))
-        assertThat(parser2.tryParse(dateTimeStrFromFormula()))
+        assertThat(parser2.parse(dateTimeStrFromFormula()))
             .isEqualTo(LocalDateTime.of(2021, 11, 15, 15, 43))
     }
 }
